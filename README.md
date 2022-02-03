5036f72e691ef25a20d80026f193af3b3c822eca
areeghyari <areegbazbaz@gmail.com>
2022-02-03 21:01:07 +0200

    first

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
 # Movies-Library
+
+# Project Name: movies-library - Project Version : 1.0.0
+
+**Author Name**: Areeg Al-hyari
+
+## WRRC
+<img src = "./WRRC.jpg">
+
+## Overview
+
+## Getting Started
+1- open browser 
+2-input URL => localhost:3210 of my app
+3-press inter
+4-now you in Home Page.
+
+## Project Features
+
---
+++ server.js
@@ -0,0 +1,75 @@
+`use strict`;
+
+const data = require(`./MovieData/data.json`);
+
+const express = require(`express`);
+const cors = require(`cors`);
+const server = express();
+server.use(cors());
+
+server.get(`/`, handelGet)
+server.get(`/favorite`, handelGetsec)
+server.use(handelErrorserver)
+server.get('*', handelError);
+
+
+
+
+
+// server.get (`/` , handelGet)
+
+function BigMovie(title, poster_path, overview) {
+    this.title = title;
+    this.poster_path = poster_path;
+    this.overview = overview;
+}
+
+
+
+function handelGet(request, response) {
+
+    let obj = new BigMovie(data.title, data.poster_path, data.overview);
+    return response.status(200).json(obj);
+}
+
+
+// ----
+
+
+server.get(`/favorite`, handelGetsec)
+
+function handelGetsec(request, response) {
+    return response.status(200).send("Welcome to Favorite Page");
+}
+
+
+
+// ---***---
+
+
+
+
+// server.get('*',handelError);
+
+function handelError(request, response) {
+    response.status(404).send('page not found error 404')
+}
+
+
+
+
+// # handel error 500
+
+server.use(handelErrorserver)
+
+function handelErrorserver(error ,request, response) {
+const err ={
+    status : 500,
+    message : error
+}
+response.status(500).send(err);
+}
+
+server.listen(3210,()=>{
+    console.log("here we start in our website 3210");
+})
