f5b84fe00c1256a49070731446c979d2b0b9f96c
areeghyari <areegbazbaz@gmail.com>
2022-02-06 01:31:01 +0200

    llast

--- server.js
+++ server.js
@@ -6,12 +6,8 @@ const express = require(`express`);
 const cors = require(`cors`);
 const axios = require('axios');
 const pg = require('pg');
-// const client = new pg.Client(process.env.DATABASE_URL);
+const client = new pg.Client(process.env.DATABASE_URL);
 
-const client = new pg.Client({
-    connectionString: process.env.DATABASE_URL,
-    ssl: { rejectUnauthorized: false }
-});
 
 const PORT = process.env.PORT;
 
